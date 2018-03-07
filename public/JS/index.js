d7b7671859a8bf38cfbe9d468affbe1e945f7949
Nebi <snoopafr@gmail.com>
2018-03-07 14:46:19 +0200

    greet new user, and alert others

--- public/JS/index.js
+++ public/JS/index.js
@@ -2,12 +2,13 @@ var socket = io();
 
     socket.on('connect', () => {
         console.log('Connected to server');
+        socket.on('oneMessage', function(message) {
+            console.log('oneMessage', message);
+        });
     });
 
     socket.on('disconnect', () => {
         console.log('Disconnected from server');
     });
 
-    socket.on('newMessage', function(message) {
-        console.log('newMessage', message);
-    })
+    
--- server/server.js
+++ server/server.js
@@ -15,6 +15,17 @@ var io = socketIO(server);
 io.on('connection', function (socket) {
     console.log('New user connected');
 
+    socket.emit('oneMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app'
+    });
+
+    socket.broadcast.emit('oneMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chatt app',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (newMessage) => {
         console.log('createMessage', newMessage);
         io.emit('newMessage', {
@@ -22,6 +33,14 @@ io.on('connection', function (socket) {
             text: newMessage.text,
             createdat: new Date().getTime()
         });
+
+       /*socket.broadcast.emit('newMessage', {
+           from: newMessage.from,
+           text: newMessage.text,
+           createdAt: new Date().getTime()
+       });*/
+
+       
     });
 
     socket.on('disconnect', function () {
